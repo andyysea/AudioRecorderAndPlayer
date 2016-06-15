ffd034b2c18796eb4df956c8b0f4822f0e1d9198
YangYingHai <601503988@qq.com>
2016-06-15 11:44:05 +0800

    创建主控制器，并且添加主控制器

---
+++ 网易新闻/网易新闻/Classes/Home/WYHomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  WYHomeViewController.h
+//  网易新闻
+//
+//  Created by 杨应海 on 16/6/15.
+//  Copyright © 2016年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WYHomeViewController : UIViewController
+
+@end
---
+++ 网易新闻/网易新闻/Classes/Main/WYMainViewController.h
@@ -0,0 +1,13 @@
+//
+//  WYMainViewController.h
+//  网易新闻
+//
+//  Created by 杨应海 on 16/6/15.
+//  Copyright © 2016年 itcast. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WYMainViewController : UITabBarController
+
+@end
